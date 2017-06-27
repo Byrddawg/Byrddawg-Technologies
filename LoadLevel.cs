ac49e7bc54c5dd67251f48a93789767ca402bc3c
Byrddawg <shane_byrd1991@hotmail.com>
2017-06-27 04:48:02 -0600

    Delete LoadLevel.cs

--- LoadLevel.cs
+++
@@ -1,9 +0,0 @@
-//Created by Shane Byrd @ Byrddawg Technologies\\
-//This code is an example of problem solving\\
-using UnityEngine;
-using System.Collections;
-using UnityEngine.SceneManagement;
-
-public class LoadLevel : MonBehaviour {
-public void Level(string name) { 
-  Application.LoadLevel("Example")
